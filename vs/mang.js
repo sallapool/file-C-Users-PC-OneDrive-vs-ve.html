efe4387b20bb848ccbdccbc270617f4bba42cc3c
sallapool <140289827+sallapool@users.noreply.github.com>
2023-12-09 02:26:19 +0300

    Add files via upload

---
+++ vs/mang.js
@@ -0,0 +1,23 @@
+let img= document.querySelector('.img');
+let containar= document.querySelector('.containar');
+
+function phones(phone){
+        img.src =phone;
+}
+function cursor(cursor){
+        
+containar.style.background=cursor;
+
+
+
+
+
+        background
+
+
+
+
+}
+
+
+
